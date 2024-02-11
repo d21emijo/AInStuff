3718b351273e08dc3a74f61933b69977d3fdccec
d21emijo <d21emijo@student.his.se>
2024-02-11 17:24:08 +0100

    Solver
    agent to solve

--- maze.js
+++ maze.js
@@ -3,5 +3,7 @@ canvas.height=window.innerHeight;
 canvas.width=200;
 
 const ctx = canvas.getContext("2d");
-const mazeSolver= new mazeSolver(100,100,30,50);
+const mazeSolver = new MazeSolver(50,50,25,15);
 mazeSolver.draw(ctx);
+
+
--- mazeSolver.js
+++ mazeSolver.js
@@ -1,18 +1,24 @@
-class mazeSolver{
+class MazeSolver{
     constructor(x,y,witdh,height){
         this.x=x;
-        this.x=x;
-        this.height=height;
+        this.y=y;
         this.witdh=witdh;
-
+        this.height=height;
     }
     draw(ctx){
         ctx.beginPath();
-        ctx.rect(
-            this.x-this.witdh/2,
-            this.x-this.height/2,
-            this.witdh,
-            this.height
+        // ctx.rect(
+        //     this.x-this.witdh/2, //placement
+        //     this.x-this.height/2, //placement
+        //     this.witdh, // witdh of solver
+        //     this.height // -- of solver
+        // );
+        ctx.arc(
+            this.x-this.witdh/2, //placement
+            this.y-this.height/2, //placement
+            15,
+            0,
+            2*Math.PI
         );
         ctx.fill();
     }
