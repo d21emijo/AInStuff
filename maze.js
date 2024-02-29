130623b4ffbc37084b8b1721e5f773ae842fb7bc
d21emijo <d21emijo@student.his.se>
2024-02-29 13:32:59 +0100

    update

--- maze.js
+++ maze.js
@@ -7,28 +7,18 @@ const mazeSolver = new MazeSolver(20,20,0,0,0); //(x,y,witdh,height,angle)
 // mazeSolver.draw(ctx);
 
 animate();
-// randomfunc();
 let newAngle;
 function randomfunc(){
-    // let newAngle=0;
-
-    newAngle=1*Math.random();
+    newAngle=2*Math.PI*Math.random();
     console.log("randomfunc",newAngle);
     console.log("KLICK");
+    animate(newAngle)
 }
 
 function animate(newAngle){
     ctx.clearRect(0,0,canvas.width,canvas.height)
-    newAngle=0;
-    // console.log(newAngle)
-    
     canvas.height=window.innerHeight*.8; // change to segmentetttttttttts
     mazeSolver.draw(ctx,newAngle);
 
-    test = document.getElementById("testing");
-    test.addEventListener("click", () =>{
-        console.log("clkiak2")
-        console.log("i animate",newAngle);
-        requestAnimationFrame(animate);
-    });
+    // requestAnimationFrame(animate)
 }
