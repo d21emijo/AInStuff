e530bcb4693c53c75f9bdfde234ca5c0dec9d7a1
d21emijo <102797583+d21emijo@users.noreply.github.com>
2024-03-24 20:19:27 +0100

    Added static points
    added hardcoded points to see if it works

--- js/maze/maze.js
+++ js/maze/maze.js
@@ -1,5 +1,5 @@
 const canvas=document.getElementById("myCanvas");
-canvas.width=200;
+canvas.width=1200;
 
 const ctx = canvas.getContext("2d");
 const mazeSolver = new MazeSolver(20,20,0,0,0); //(x,y,witdh,height,angle)
@@ -18,6 +18,8 @@ function animate(newAngle){
     ctx.clearRect(0,0,canvas.width,canvas.height)
     canvas.height=window.innerHeight*.8; // change to segmentetttttttttts
     mazeSolver.draw(ctx,newAngle);
-    console.log("animate", newAngle)
-    requestAnimationFrame(animate)
+    console.log("animate", newAngle);
+    graph.draw(ctx);
+    console.log(p1);
+    // requestAnimationFrame(animate)
 }
